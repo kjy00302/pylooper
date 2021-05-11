52d4da2a452e8deaa8495fe6325fcfa8e72bed57
kjy00302 <kjy00302@gmail.com>
2021-05-11 22:41:37 +0900

    Add main and (empty) gui

---
+++ pylooper/__main__.py
@@ -0,0 +1,27 @@
+from .aw_looper import Looper
+from .gui import GUI
+
+class GUI(GUI):
+    def cb_stop(self):
+        looper.stop()
+
+    def cb_rec(self):
+        looper.record()
+
+    def cb_play(self):
+        looper.play()
+
+    def cb_overdub(self):
+        looper.overrec()
+
+if __name__ == "__main__":
+    GUI.gui_init()
+
+    looper = Looper()
+    gui = GUI()
+
+    looper.aw_start()
+    gui.gui_run()
+
+    GUI.gui_deinit()
+    looper.aw_stop()
---
+++ pylooper/gui.py
@@ -0,0 +1,37 @@
+from .guibase import GUIBase
+import pygame
+import pygame.locals
+
+class GUI(GUIBase):
+    def __init__(self):
+        super().__init__((100,100))
+
+    def gui_event(self, event):
+        if event.type == pygame.locals.QUIT:
+            self.gui_running = False
+        elif event.type == pygame.locals.KEYDOWN:
+            if event.key == pygame.locals.K_1:
+                self.cb_stop()
+            elif event.key == pygame.locals.K_2:
+                self.cb_rec()
+            elif event.key == pygame.locals.K_3:
+                self.cb_play()
+            elif event.key == pygame.locals.K_4:
+                self.cb_overdub()
+            elif event.key == pygame.locals.K_ESCAPE:
+                self.gui_running = False
+
+    def gui_draw(self, surface):
+        surface.fill((255,255,255))
+
+    def cb_stop(self):
+        raise NotImplementedError
+
+    def cb_rec(self):
+        raise NotImplementedError
+
+    def cb_play(self):
+        raise NotImplementedError
+
+    def cb_overdub(self):
+        raise NotImplementedError
