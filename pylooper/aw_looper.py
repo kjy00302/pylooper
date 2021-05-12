e0a60da0f6dedeaecb4ea1fd939a1a6edd0b7f0e
kjy00302 <kjy00302@gmail.com>
2021-05-12 12:36:35 +0900

    Add some kind of audio blanker

--- pylooper/aw_looper.py
+++ pylooper/aw_looper.py
@@ -15,8 +15,13 @@ class Looper(audioworker.AudioWorker):
         self._aud_buf = array.array('h')
         self._chunk_cnt = 0
         self._chunk_ptr = 0
+        self._rms = 0
 
     def aw_callback(self, in_data, frame_count, time_info, status_flags):
+        self._rms = self._rms * 0.7 + audioop.rms(in_data, 2)/32768 * 100 * 0.3
+        if self._rms < 5:
+            in_data = audioop.mul(in_data, 2, (self._rms/5) ** 8)
+
         if self._state & 2: # LOOPER_PLAY & LOOPER_OVERDUB
             offset = self._bufsize * self._chunk_ptr
             if self._state == LOOPER_OVERDUB:
