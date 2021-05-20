c710b28eddd5bf621d1c2a253e5a4d6f78b26640
kjy00302 <kjy00302@gmail.com>
2021-05-20 20:22:42 +0900

    Add stream initiation check

--- pylooper/audioworker.py
+++ pylooper/audioworker.py
@@ -4,23 +4,32 @@ class AudioWorker:
     def __init__(self, samplerate, bufsize=1024):
         self._samplerate = samplerate
         self._bufsize = bufsize
-        
         self._pa = pyaudio.PyAudio()
-        self.stream = self._pa.open(
-            format=pyaudio.paInt16,
-            channels=1,
-            rate=samplerate,
-            output=True,
-            input=True,
-            frames_per_buffer=bufsize,
-            stream_callback=self.aw_callback
-        )
+
+    def aw_init(self):
+        try:
+            self.stream = self._pa.open(
+                format=pyaudio.paInt16,
+                channels=1,
+                rate=self._samplerate,
+                output=True,
+                input=True,
+                frames_per_buffer=self._bufsize,
+                stream_callback=self.aw_callback
+            )
+        except Exception as e:
+            print("Failed to open audio stream")
+            print("Error:",type(e), e)
+            if e.errno == -9996:
+                print("Check your audio device")
+            return False
         self.chunk_latency = int(
             self._samplerate * (
                 self.stream.get_input_latency()
                 + self.stream.get_input_latency()
-            ) / bufsize
+            ) / self._bufsize
         )
+        return True
 
     def aw_callback(self, in_data, frame_count, time_info, status_flags):
         raise NotImplementedError
