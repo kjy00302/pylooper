0716f17c2e7ad8c0545410870fd2785ab10edc13
kjy00302 <kjy00302@gmail.com>
2021-05-23 20:04:28 +0900

    Rework config reader

--- pylooper/configuration.py
+++ pylooper/configuration.py
@@ -1,47 +1,38 @@
 import configparser
+import pygame.locals
 
 DEFAULT ={
-    'title' : 'Looper',
-    'volume' : '50',
-    'stop' : 'K_1',
-    'record' : 'K_2',
-    'play' : 'K_3',
-    'overdub' : 'K_4',
-    'samplerate' : '48000'}
-
-def config_generator():
-    #make config file
-    config = configparser.ConfigParser()
-
-    # make config object
-    config['audio'] = DEFAULT
-    
-
-    #save config file
-    with open('audio.ini', 'w') as audiofile:
-        config.write(audiofile)
+    'Audio':{
+        'samplerate' : 48000
+    },
+    'Keymaps': {
+        'key_stop' : 'K_1',
+        'key_record' : 'K_2',
+        'key_play' : 'K_3',
+        'key_overdub' : 'K_4'
+    }
+}
 
 def config_read():
     #read config file
     config = configparser.ConfigParser()
-    try:
-        config.read('audio.ini')
-    except FileNotFoundError:
-        config_generator()
-    else:
-        #confirm config file sction
-        config.sections()
-        ini_read(config)
+    config.read('config.ini')
+    if not config.has_section('Audio'):
+        config.read_dict(DEFAULT)
+        with open('config.ini', 'w') as audiofile:
+            config.write(audiofile)
+        return config
+    #confirm config file sction
+    if not config_check(config):
+        config.read_dict(DEFAULT)
+    return config
 
-def ini_read(config):
+def config_check(config):
     #read section
-    title = config['audio']['title']
-    volume = config['audio']['volume']
-    stop = config['audio']['stop']
-    record = config['audio']['record']
-    play = config['audio']['play']
-    overdub = config['audio']['overdub']
-    samplerate = config['audio']['samplerate']
-
-config_generator()
-config_read()
+    try:
+        for k, v in config.items('Keymaps'):
+            getattr(pygame.locals, v)
+        config.getint('Audio', 'samplerate')
+    except:
+        return False
+    return True
